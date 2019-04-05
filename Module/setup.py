36a0e5385ed414e5da794648caaf3368cfd94b5b
Bahati <gur79tub@gmail.com>
2019-04-05 12:14:37 +0200

    added setup.py

---
+++ Module/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='mymath',
+    version='0.1',
+    description='A silly math package',
+    author='Me baby',
+    author_email='me@me.com',
+    url='http:www.url.com',
+    packages=['mymath', 'mymath.adv'],
+)
